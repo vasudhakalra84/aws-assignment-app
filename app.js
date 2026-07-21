fc8f45046a64ab150a799effc754b14ba1fa1a82
Vasudha Kalra <vasudha.kalra@nagarro.com>
2026-07-21 12:13:06 +0530

    Initial comit

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('AWS Assignment Running');
+});
+
+app.get('/health', (req, res) => {
+  res.json({
+    status: 'UP'
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
